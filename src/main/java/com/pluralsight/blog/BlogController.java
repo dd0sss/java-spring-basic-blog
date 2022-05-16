00ba6ce17f24f339a715936b2e04f73d630318bd
dd0sss <63234076+dd0sss@users.noreply.github.com>
2022-05-16 11:43:31 +0200

    added my project

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,28 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+    //init the posts repo
+    private PostRepository postRepository;
 
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 
     @RequestMapping("/")
     public String listPosts( ModelMap modelMap){
-        modelMap.put("title","Blog Post 1");
+//store the posts in a list posts from the post repo
+        List<Post>posts = postRepository.getAllPosts();
+        //display the list
+        modelMap.put("posts",posts);
         return "home";
     }
 
